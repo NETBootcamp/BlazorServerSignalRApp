52bb3ded5e44a1ff9ff6e281e4111653dca69491
Victor Parra <vparra@apexsystems.com>
2023-08-02 21:42:58 -0600

    Add project files.

---
+++ BlazorServerSignalRApp/Hubs/CountHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorServerSignalRApp.Server.Hubs;
+
+public class CountHub : Hub
+{
+    public async Task SendMessage(int count)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", count);
+    }
+}
